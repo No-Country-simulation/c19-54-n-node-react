9024774793960896ce7898f3fdf5396b0bc2635a
Lucila Chaparro <lchaparro@dgrcorrientes.gov.ar>
2024-07-11 14:35:10 -0300

    db connection

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,12 +1,15 @@
 import { set, connect } from 'mongoose'
-const uri = "mongodb+srv://admin:u3tp3DSZjyV51x5F@e-commerce.z58ekxs.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce";
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
 set('strictQuery', true, 'useNewUrlParser', true)
 const connectDB = async () => {
   try {
     // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
-    await mongoose.connect(uri, clientOptions);
+    await mongoose.connect(process.env.MONGO_URI, clientOptions);
     await mongoose.connection.db.admin().command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (err) {
