6bcc9cfc4e500612b0effa7d8f28bad5ab4f373e
Lucila Chaparro <lchaparro@dgrcorrientes.gov.ar>
2024-07-11 14:29:50 -0300

    db connection

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,12 +1,17 @@
 import { set, connect } from 'mongoose'
+const uri = "mongodb+srv://admin:u3tp3DSZjyV51x5F@e-commerce.z58ekxs.mongodb.net/?retryWrites=true&w=majority&appName=e-commerce";
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+
 set('strictQuery', true, 'useNewUrlParser', true)
 const connectDB = async () => {
   try {
-    await connect(process.env.URI)
-    console.log('MongoDB is Connected...')
+    // Create a Mongoose client with a MongoClientOptions object to set the Stable API version
+    await mongoose.connect(uri, clientOptions);
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } catch (err) {
     console.error(err.message)
-    process.exit(1)
+    await mongoose.disconnect();
   }
 }
 export default connectDB
