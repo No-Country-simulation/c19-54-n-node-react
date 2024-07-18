8c10526dc27526178a4e6230f7d1ff64dc0f59fe
Lucila Chaparro <lucilachaparro@gmail.com>
2024-07-17 23:16:30 -0300

    createProduct controller

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,4 +1,6 @@
 import { Product } from '../models/Product.js'
+import { uploadFile } from '../utils/uploadFile.js'
+import { upload } from '../config/multer.js'
 
 const getAllProducts = async (req, res) => {
 
@@ -9,8 +11,52 @@ const getProductById = async (req, res) => {
 }
 
 const createProduct = async (req, res) => {
-
+    upload.fields([{ name: 'images', maxCount: 5 }])(req, res, async err => {
+      if (err) {
+        return res.status(500).json({ status: 'Failed', message: err.message })
+      }
+      try {
+        const body = req.body
+        const images = req.files ? req.files.images : null
+  
+        console.log(body)
+  
+        if (images && images.length > 0) {
+  
+          let imageArray = []
+          for (let i = 0; i < images.length; i++) {
+          let { downloadURL } = await uploadFile(images[i])
+          imageArray.push(downloadURL)
+          }
+  
+          const newProduct = new Product({
+            name: body.name,
+            description: body.description,
+            originalPrice: body.originalPrice,
+            categories: body.categories.split(','),
+            status: "Unpublished",
+            images: imageArray,
+            storeId: "21"//placeholder, required property
+          })
+          await newProduct.save()
+          return res.status(200).json({
+            status: 'Success',
+            data: { newProduct }
+          })
+        } else {
+          return res
+            .status(400)
+            .json({ status: 'Failed', message: 'Debes enviar una imagen' })
+        }
+      } catch (err) {
+        res.status(500).json({
+          status: 'Failed',
+          message: err.message
+        })
+      }
+    })
 }
+
 
 const updateProduct = async (req, res) => {
 
