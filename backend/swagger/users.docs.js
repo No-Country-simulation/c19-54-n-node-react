38ebc0faff18c4eee917f279c0739916e4e653d1
valentttino <vgboggio@gmail.com>
2024-07-20 17:56:55 -0300

    act users doc

--- backend/swagger/users.docs.js
+++ backend/swagger/users.docs.js
@@ -17,74 +17,77 @@
  *                   type: string
  *                   example: Success
  *                 data:
- *                   type: array
- *                   items: 
- *                     type: object
- *                     properties:
- *                       billingAddress:
- *                         type: object
- *                         properties:
- *                           country:
- *                             type: string
- *                             example: Argentina
- *                           street1:
- *                             type: string
- *                             example: Calle1 345
- *                           city:
- *                             type: string
- *                             example: Buenos Aires
- *                           province:
- *                             type: string
- *                             example: Capital Federal
- *                           zip:
- *                             type: string
- *                             example: 35d2
- *                       shippingAddress:
- *                         type: object
- *                         properties:
- *                           country:
- *                             type: string
- *                             example: Argentina
- *                           street1:
- *                             type: string
- *                             example: Calle1 345
- *                           city:
- *                             type: string
- *                             example: Buenos Aires
- *                           province:
- *                             type: string
- *                             example: Capital Federal
- *                           zip:
- *                             type: string
- *                             example: 35d2
- *                       _id:
- *                         type: string
- *                         example: 669874be9491ccf0ebeec67a
- *                       name:
- *                         type: string
- *                         example: John Doe
- *                       email:
- *                         type: string
- *                         example: jd@mail.com
- *                       image:
- *                         type: string
- *                         example: https://firebasestorage.googleapis.com/v0/b/arte-sano.appspot.com/o/images%2F60f01a50-9ee6-457f-acd1-3dad1cf10f1c.webp?alt=media&token=aad2a33b-825b-49b4-8756-0be227e2ee11
- *                       password:
- *                         type: string
- *                         example: pastafrola
- *                       roles:
- *                           type: array
- *                           items:
- *                             type: string
- *                       createdAt:
- *                         type: string
- *                         example: 2024-07-18T16:48:06.581Z
- *                       updatedAt:
- *                         type: string
- *                         example: 2024-07-18T16:48:06.581Z
- *                       __v:
- *                         type: integer
- *                         example: 0 
+ *                   type: object
+ *                   properties:
+ *                      users:
+ *                        type: array
+ *                        items: 
+ *                          type: object
+ *                          properties:
+ *                             billingAddress:
+ *                                type: object
+ *                                properties:
+ *                                  country:
+ *                                    type: string
+ *                                    example: Argentina
+ *                                  street1:
+ *                                    type: string
+ *                                    example: Calle1 345
+ *                                  city:
+ *                                    type: string
+ *                                    example: Buenos Aires
+ *                                  province:
+ *                                    type: string
+ *                                    example: Capital Federal
+ *                                  zip:
+ *                                    type: string
+ *                                    example: 35d2
+ *                             shippingAddress:
+ *                                type: object
+ *                                properties:
+ *                                  country:
+ *                                    type: string
+ *                                    example: Argentina
+ *                                  street1:
+ *                                    type: string
+ *                                    example: Calle1 345
+ *                                  city:
+ *                                    type: string
+ *                                    example: Buenos Aires
+ *                                  province:
+ *                                    type: string
+ *                                    example: Capital Federal
+ *                                  zip:
+ *                                    type: string
+ *                                    example: 35d2
+ *                             _id:
+ *                                type: string
+ *                                example: 669874be9491ccf0ebeec67a
+ *                             name:
+ *                                type: string
+ *                                example: John Doe
+ *                             email:
+ *                                type: string
+ *                                example: jd@mail.com
+ *                             image:
+ *                                type: string
+ *                                example: https://firebasestorage.googleapis.com/v0/b/arte-sano.appspot.com/o/images%2F60f01a50-9ee6-457f-acd1-3dad1cf10f1c.webp?alt=media&token=aad2a33b-825b-49b4-8756-0be227e2ee11
+ *                             password:
+ *                                type: string
+ *                                example: pastafrola
+ *                             roles:
+ *                                type: array
+ *                                items:
+ *                                  type: string
+ *                             createdAt:
+ *                                type: string
+ *                                example: 2024-07-18T16:48:06.581Z
+ *                             updatedAt:
+ *                                type: string
+ *                                example: 2024-07-18T16:48:06.581Z
+ *                             __v:
+ *                                type: integer
+ *                                example: 0 
  *       500:
  *          description: Error
  *          content:    
@@ -144,55 +147,75 @@
  *                  status: 
  *                     type: string
  *                  data:
- *                     type: object
- *                     properties:
- *                         billingAddress:
- *                           type: object
- *                           properties:
- *                             country:
- *                               type: string
- *                             street1:
- *                               type: string
- *                             city:
- *                               type: string
- *                             province:
- *                               type: string
- *                             zip:
- *                               type: string
- *                         shippingAddress:
- *                           type: object
- *                           properties:
- *                             country:
- *                               type: string
- *                             street1:
- *                               type: string
- *                             city:
- *                               type: string
- *                             province:
- *                               type: string
- *                             zip:
- *                               type: string
- *                         _id:
- *                           type: string
- *                         name:
- *                           type: string
- *                         email:
- *                           type: string
- *                         image:
- *                           type: string
- *                         password:
- *                           type: string
- *                         roles:
- *                           type: array
- *                           items:
- *                             type: string
- *                         createdAt:
- *                           type: string
- *                         updateAt:
- *                           type: string
- *                         __v:
- *                           type: integer
- *                           example: 0  
+ *                   type: object
+ *                   properties:
+ *                      user: 
+ *                          type: object
+ *                          properties:
+ *                             billingAddress:
+ *                                type: object
+ *                                properties:
+ *                                  country:
+ *                                    type: string
+ *                                    example: Argentina
+ *                                  street1:
+ *                                    type: string
+ *                                    example: Calle1 345
+ *                                  city:
+ *                                    type: string
+ *                                    example: Buenos Aires
+ *                                  province:
+ *                                    type: string
+ *                                    example: Capital Federal
+ *                                  zip:
+ *                                    type: string
+ *                                    example: 35d2
+ *                             shippingAddress:
+ *                                type: object
+ *                                properties:
+ *                                  country:
+ *                                    type: string
+ *                                    example: Argentina
+ *                                  street1:
+ *                                    type: string
+ *                                    example: Calle1 345
+ *                                  city:
+ *                                    type: string
+ *                                    example: Buenos Aires
+ *                                  province:
+ *                                    type: string
+ *                                    example: Capital Federal
+ *                                  zip:
+ *                                    type: string
+ *                                    example: 35d2
+ *                             _id:
+ *                                type: string
+ *                                example: 669874be9491ccf0ebeec67a
+ *                             name:
+ *                                type: string
+ *                                example: John Doe
+ *                             email:
+ *                                type: string
+ *                                example: jd@mail.com
+ *                             image:
+ *                                type: string
+ *                                example: https://firebasestorage.googleapis.com/v0/b/arte-sano.appspot.com/o/images%2F60f01a50-9ee6-457f-acd1-3dad1cf10f1c.webp?alt=media&token=aad2a33b-825b-49b4-8756-0be227e2ee11
+ *                             password:
+ *                                type: string
+ *                                example: pastafrola
+ *                             roles:
+ *                                type: array
+ *                                items:
+ *                                  type: string
+ *                             createdAt:
+ *                                type: string
+ *                                example: 2024-07-18T16:48:06.581Z
+ *                             updatedAt:
+ *                                type: string
+ *                                example: 2024-07-18T16:48:06.581Z
+ *                             __v:
+ *                                type: integer
+ *                                example: 0  
  *       400:
  *          description: Error
  *          content:    
@@ -245,53 +268,73 @@
  *                 data:
  *                   type: object
  *                   properties:
- *                       billingAddress:
- *                         type: object
- *                         properties:
- *                           country:
- *                             type: string
- *                           street1:
- *                             type: string
- *                           city:
- *                             type: string
- *                           province:
- *                             type: string
- *                           zip:
- *                             type: string
- *                       shippingAddress:
- *                         type: object
- *                         properties:
- *                           country:
- *                             type: string
- *                           street1:
- *                             type: string
- *                           city:
- *                             type: string
- *                           province:
- *                             type: string
- *                           zip:
- *                             type: string
- *                       _id:
- *                         type: string
- *                       name:
- *                         type: string
- *                       email:
- *                         type: string
- *                       image:
- *                         type: string
- *                       password:
- *                         type: string
- *                       roles:
- *                         type: array
- *                         items:
- *                           type: string
- *                       createdAt:
- *                         type: string
- *                       updateAt:
- *                         type: string
- *                       __v:
- *                         type: integer
- *                         example: 0
+ *                      user: 
+ *                          type: object
+ *                          properties:
+ *                             billingAddress:
+ *                                type: object
+ *                                properties:
+ *                                  country:
+ *                                    type: string
+ *                                    example: Argentina
+ *                                  street1:
+ *                                    type: string
+ *                                    example: Calle1 345
+ *                                  city:
+ *                                    type: string
+ *                                    example: Buenos Aires
+ *                                  province:
+ *                                    type: string
+ *                                    example: Capital Federal
+ *                                  zip:
+ *                                    type: string
+ *                                    example: 35d2
+ *                             shippingAddress:
+ *                                type: object
+ *                                properties:
+ *                                  country:
+ *                                    type: string
+ *                                    example: Argentina
+ *                                  street1:
+ *                                    type: string
+ *                                    example: Calle1 345
+ *                                  city:
+ *                                    type: string
+ *                                    example: Buenos Aires
+ *                                  province:
+ *                                    type: string
+ *                                    example: Capital Federal
+ *                                  zip:
+ *                                    type: string
+ *                                    example: 35d2
+ *                             _id:
+ *                                type: string
+ *                                example: 669874be9491ccf0ebeec67a
+ *                             name:
+ *                                type: string
+ *                                example: John Doe
+ *                             email:
+ *                                type: string
+ *                                example: jd@mail.com
+ *                             image:
+ *                                type: string
+ *                                example: https://firebasestorage.googleapis.com/v0/b/arte-sano.appspot.com/o/images%2F60f01a50-9ee6-457f-acd1-3dad1cf10f1c.webp?alt=media&token=aad2a33b-825b-49b4-8756-0be227e2ee11
+ *                             password:
+ *                                type: string
+ *                                example: pastafrola
+ *                             roles:
+ *                                type: array
+ *                                items:
+ *                                  type: string
+ *                             createdAt:
+ *                                type: string
+ *                                example: 2024-07-18T16:48:06.581Z
+ *                             updatedAt:
+ *                                type: string
+ *                                example: 2024-07-18T16:48:06.581Z
+ *                             __v:
+ *                                type: integer
+ *                                example: 0
  *       '400':
  *         description: Error - Invalid user ID
  *         content:
