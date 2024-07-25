17160b9095f0757e80facc90e734236e28dc8d40
Lucila Chaparro <lchaparro@dgrcorrientes.gov.ar>
2024-07-25 15:37:02 -0300

    token verification

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,15 +1,16 @@
 import { Router } from 'express'
 import { controllers } from '../controllers/user.controller.js'
+import { tControllers } from '../controllers/verifyToken.controller.js'
 
 
 const UsersRouter = Router()
 
 UsersRouter
-    .get('/', controllers.getAllUsers)
-    .get('/:id', controllers.getUserById)
-    .post('/', controllers.createUser)
-    .put('/:id', controllers.updateUser)
-    .delete('/:id', controllers.deleteUser)
-    .get('/role/:role', controllers.getUsersByRole)
+    .get('/', tControllers.verifyTokenAndAdmin, controllers.getAllUsers)
+    .get('/:id', tControllers.verifyToken, controllers.getUserById)
+    .post('/', tControllers.verifyToken, controllers.createUser)
+    .put('/:id, ', tControllers.verifyTokenAndAuthorization, controllers.updateUser)
+    .delete('/:id', tControllers.verifyTokenAndAuthorization, controllers.deleteUser)
+    .get('/role/:role', tControllers.verifyToken, controllers.getUsersByRole)
 
 export default UsersRouter
