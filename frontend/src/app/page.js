d02d1410c3c2dd1dc2ac736c951cd2ee546974b2
Carlos <musicarlos72@gmail.com>
2024-07-30 20:51:55 -0300

    registro vendedor-1

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,4 +1,4 @@
-import Carrousel from "@/components/Carrousel";
+import Carrousel from "../components/carrousel";
 import Link from "next/link";
 
 export default function Home() {
@@ -24,29 +24,29 @@ export default function Home() {
   return (
     <main>
       <div className="bg-[url('../../public/banner.jpg')] h-[200px] md:h-[400px] lg:h-[600px] bg-norepeat bg-cover">
-        <div className="flex flex-col lg:p-6 lg:gap-10 text-center text-brown lg:font-bold md:text-2xl gap-4 md:gap-14 md:ml-5 lg:text-4xl lg:w-[50%] md:w-[50%] w-[50%] text-[12px]">
-          <h1 className="bg-cream bg-opacity-30 rounded-lg lg:p-3 md:p-3 p-1 font-bold md:mt-5 lg:mt-[24%]">
+        <div className="flex flex-col pt-6 lg:p-6 lg:gap-10 text-center text-brown lg:font-bold md:text-xl gap-4 md:gap-14 md:ml-5 lg:text-2xl lg:w-[50%] md:w-[50%] w-[50%] text-[12px]">
+          <h1 className="bg-cream bg-opacity-60 rounded-lg lg:p-3 md:p-3 p-1 font-bold md:mt-5 lg:mt-[24%]">
             Portal de venta de artesanías
           </h1>
-          <p className="bg-cream font-bold bg-opacity-30 rounded-lg lg:p-3 md:p-3 p-1 ">
+          <p className="bg-cream font-bold bg-opacity-60 rounded-lg lg:p-3 md:p-3 p-1 ">
             Espacio dedicado con Amor para vincular Artesanos, con amantes del
             arte y la belleza
           </p>
           <div className="flex lg:flex-row lg:justify-between md:flex-row md:justify-between md:gap-5 gap-5 font-bold">
-            <Link href={"/products"}>
-            <button className="bg-cream bg-opacity-30 rounded-lg p-2 text-nowrap">
-              Ver Productos
-            </button>
+            <Link href={"/#products"}>
+              <button className="bg-cream bg-opacity-60 rounded-lg p-2 text-nowrap xl:text-xl hover:shadow-xl hover:shadow-amber">
+                Ver Productos
+              </button>
             </Link>
-            <Link href={"/sellers"}>
-            <button className="bg-cream bg-opacity-30 rounded-lg p-2 text-nowrap">
-              ¿Sos Vendedor?
-            </button>
+            <Link href={"/SellerRegister"}>
+              <button className="bg-cream bg-opacity-60 rounded-lg p-2 text-nowrap xl:text-xl hover:shadow-xl hover:shadow-amber">
+                ¿Sos Vendedor?
+              </button>
             </Link>
           </div>
         </div>
       </div>
-      <section className="bg-cream">
+      <section className="bg-cream" id="products">
         {categories.map(({ Category }, index) => (
           <Carrousel key={index} categoryName={Category} />
         ))}
