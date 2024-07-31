a377ae0173b4f56c725eba212231c6d3237c5e38
Mati Pretz <30426732+matipretz@users.noreply.github.com>
2024-07-31 17:57:01 -0300

    no front

--- frontend/src/app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main>
-        <div className="bg-[url('../../public/banner.jpg')] h-[600px] bg-norepeat bg-cover">
-        </div>
-    </main>
-  );
-}
