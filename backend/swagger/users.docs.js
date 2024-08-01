120f76ba0f26a236bc25e81622ba94bb5e780aae
valentttino <vgboggio@gmail.com>
2024-07-31 22:02:00 -0300

    update user docs

--- backend/swagger/users.docs.js
+++ backend/swagger/users.docs.js
@@ -361,4 +361,359 @@
  *                 message:
  *                   type: string
  *                   example: User not found
+ * 
+ *   put:
+ *     tags:
+ *       - Users
+ *     summary: Update a user by ID
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the user
+ *         schema:
+ *           type: string
+ *           example: 60d21b4667d0d8992e610c85
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: John Doe
+ *               email:
+ *                 type: string
+ *                 example: john.doe@example.com
+ *               image:
+ *                 type: string
+ *                 example: /path/to/image.jpg
+ *               password:
+ *                 type: string
+ *                 example: new_password
+ *               billingAddress:
+ *                 type: object
+ *                 properties:
+ *                   country:
+ *                     type: string
+ *                     example: USA
+ *                   street1:
+ *                     type: string
+ *                     example: 123 Main St
+ *                   street2:
+ *                     type: string
+ *                     example: Apt 4B
+ *                   city:
+ *                     type: string
+ *                     example: New York
+ *                   province:
+ *                     type: string
+ *                     example: NY
+ *                   zip:
+ *                     type: string
+ *                     example: 10001
+ *               shippingAddress:
+ *                 type: object
+ *                 properties:
+ *                   country:
+ *                     type: string
+ *                     example: USA
+ *                   street1:
+ *                     type: string
+ *                     example: 123 Main St
+ *                   street2:
+ *                     type: string
+ *                     example: Apt 4B
+ *                   city:
+ *                     type: string
+ *                     example: New York
+ *                   province:
+ *                     type: string
+ *                     example: NY
+ *                   zip:
+ *                     type: string
+ *                     example: 10001
+ *               roles:
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 example: ["user", "admin"]
+ *     responses:
+ *       '200':
+ *         description: User successfully updated
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     user:
+ *                       type: object
+ *                       properties:
+ *                         _id:
+ *                           type: string
+ *                           example: 60d21b4667d0d8992e610c85
+ *                         name:
+ *                           type: string
+ *                           example: John Doe
+ *                         email:
+ *                           type: string
+ *                           example: john.doe@example.com
+ *                         image:
+ *                           type: string
+ *                           example: /path/to/image.jpg
+ *                         billingAddress:
+ *                           type: object
+ *                           properties:
+ *                             country:
+ *                               type: string
+ *                               example: USA
+ *                             street1:
+ *                               type: string
+ *                               example: 123 Main St
+ *                             street2:
+ *                               type: string
+ *                               example: Apt 4B
+ *                             city:
+ *                               type: string
+ *                               example: New York
+ *                             province:
+ *                               type: string
+ *                               example: NY
+ *                             zip:
+ *                               type: string
+ *                               example: 10001
+ *                         shippingAddress:
+ *                           type: object
+ *                           properties:
+ *                             country:
+ *                               type: string
+ *                               example: USA
+ *                             street1:
+ *                               type: string
+ *                               example: 123 Main St
+ *                             street2:
+ *                               type: string
+ *                               example: Apt 4B
+ *                             city:
+ *                               type: string
+ *                               example: New York
+ *                             province:
+ *                               type: string
+ *                               example: NY
+ *                             zip:
+ *                               type: string
+ *                               example: 10001
+ *                         roles:
+ *                           type: array
+ *                           items:
+ *                             type: string
+ *                           example: ["user", "admin"]
+ *                         createdAt:
+ *                           type: string
+ *                           example: 2024-07-31T12:34:56.789Z
+ *                         updatedAt:
+ *                           type: string
+ *                           example: 2024-07-31T12:34:56.789Z
+ *       '400':
+ *         description: Invalid user ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Failed
+ *                 message:
+ *                   type: string
+ *                   example: Invalid user ID
+ *       '404':
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Failed
+ *                 message:
+ *                   type: string
+ *                   example: User not found
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Failed
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ * 
+ *   delete:
+ *     tags:
+ *       - Users
+ *     summary: Delete a user by ID
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         description: The ID of the user
+ *         schema:
+ *           type: string
+ *           example: 60d21b4667d0d8992e610c85
+ *     responses:
+ *       '200':
+ *         description: User successfully deleted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: string
+ *               example: User with id = 60d21b4667d0d8992e610c85 deleted
+ *       '400':
+ *         description: Invalid user ID
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Failed
+ *                 message:
+ *                   type: string
+ *                   example: Invalid user ID
+ *       '404':
+ *         description: User not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Failed
+ *                 message:
+ *                   type: string
+ *                   example: User not found
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Failed
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
+ * 
+ * /users/role/{role}:
+ *   get:
+ *     tags:
+ *       - Users
+ *     summary: Get users by role
+ *     parameters:
+ *       - name: role
+ *         in: path
+ *         required: true
+ *         description: The role of the users to retrieve
+ *         schema:
+ *           type: string
+ *           example: admin
+ *     responses:
+ *       '200':
+ *         description: Users successfully retrieved
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Success
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     users:
+ *                       type: array
+ *                       items:
+ *                         type: object
+ *                         properties:
+ *                           _id:
+ *                             type: string
+ *                             example: 60d21b4667d0d8992e610c85
+ *                           name:
+ *                             type: string
+ *                             example: John Doe
+ *                           email:
+ *                             type: string
+ *                             example: john.doe@example.com
+ *                           image:
+ *                             type: string
+ *                             example: https://example.com/image.jpg
+ *                           roles:
+ *                             type: array
+ *                             items:
+ *                               type: string
+ *                             example: ["admin"]
+ *                           createdAt:
+ *                             type: string
+ *                             example: 2021-06-23T18:25:43.511Z
+ *                           updatedAt:
+ *                             type: string
+ *                             example: 2021-06-23T18:25:43.511Z
+ *       '400':
+ *         description: Invalid role
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Failed
+ *                 message:
+ *                   type: string
+ *                   example: Invalid role
+ *       '404':
+ *         description: Users not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Failed
+ *                 message:
+ *                   type: string
+ *                   example: Users not found
+ *       '500':
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: Failed
+ *                 message:
+ *                   type: string
+ *                   example: Internal server error
  */
